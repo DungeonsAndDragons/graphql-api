148e5539024dfc129b7989613c28efa4355858a2
Til Blechschmidt <til@blechschmidt.de>
2018-02-24 17:08:15 +0100

    Fixed syntax of LeveledClasses

--- database/tables/LeveledClasses.sql
+++ database/tables/LeveledClasses.sql
@@ -2,6 +2,6 @@ CREATE TABLE LeveledClasses (
     id         INTEGER PRIMARY KEY,
 	character  INTEGER REFERENCES Characters,
     class      INTEGER REFERENCES Classes,
-	subclass   INTEGER REFERENCES Subclasses
+	subclass   INTEGER REFERENCES Subclasses,
     level      INTEGER
 );
