91c50ae161fa19a5498f324810ea9b69af17cc75
Til Blechschmidt <til@blechschmidt.de>
2018-02-25 00:32:37 +0100

    Added password field to players

---
+++ database/data/Players.sql
@@ -0,0 +1,4 @@
+INSERT INTO Players (name, password) VALUES
+("Max", "38221f3553236a28300b859c399a6e0cdc691b7c625bf23162c79c241a65635a348ffeb27f47a20cba5ee7a7c67ac2dfb686100dcd3abd7e4663cfacd9b25f80"),
+("Jan", "38221f3553236a28300b859c399a6e0cdc691b7c625bf23162c79c241a65635a348ffeb27f47a20cba5ee7a7c67ac2dfb686100dcd3abd7e4663cfacd9b25f80");
+-- passwords are 'supersecret'
--- database/tables/Players.sql
+++ database/tables/Players.sql
@@ -1,6 +1,7 @@
 CREATE TABLE Players(
-    id     INTEGER PRIMARY KEY,
-    name   TEXT NOT NULL DEFAULT "",
-    email  TEXT,
-    isGM   BOOLEAN NOT NULL DEFAULT 0
+    id          INTEGER PRIMARY KEY,
+    name        TEXT NOT NULL DEFAULT "",
+    email       TEXT,
+    isGM        BOOLEAN NOT NULL DEFAULT 0,
+    password    TEXT NOT NULL -- SHA512 hex hash
 );
